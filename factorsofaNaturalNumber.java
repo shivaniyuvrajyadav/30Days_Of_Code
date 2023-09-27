29ae40329b471a0b7f21cf9f3c5125736b678afd
shivani yadav <112896644+shivaniyuvrajyadav@users.noreply.github.com>
2023-09-27 12:24:46 +0530

    Create factorsofaNaturalNumber.java

---
+++ factorsofaNaturalNumber.java
@@ -0,0 +1,13 @@
+import java .util.Scanner;
+public class factorsofaNaturalNumber {
+public static void main (String []args) {
+	Scanner sc = new Scanner(System.in);
+	int a = sc.nextInt();
+	for(int i = 1;i<=a;i++) {
+		if(a%i==0) {
+			System.out.println(i);
+		}
+	}
+	sc.close();
+}
+}
