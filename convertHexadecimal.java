5eaad653af6e972b66981cb51f382436002845ef
shivani yadav <112896644+shivaniyuvrajyadav@users.noreply.github.com>
2023-09-27 14:56:05 +0530

    convertHexadecimal.java

---
+++ convertHexadecimal.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String toHex(int num) {
+        String hexValue = Integer.toHexString(num);
+        return hexValue;
+    }
+}
+// 
+// The code is a Java class named Solution that contains a method called toHex.
+
+// The toHex method takes an integer num as input and converts it into its corresponding hexadecimal representation as a string.
+
+// Inside the method, the Integer.toHexString method is used to convert the num into a hexadecimal string representation. The resulting string is stored in the variable hexValue.
+
+// Finally, the toHex method returns the hexValue string, which represents the hexadecimal equivalent of the input num. /*
