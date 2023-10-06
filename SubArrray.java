dcde50730eea57af072599b4023eabbcd2754636
shivani yadav <112896644+shivaniyuvrajyadav@users.noreply.github.com>
2023-10-06 14:14:21 +0530

    Create SubArrray.java

---
+++ SubArrray.java
@@ -0,0 +1,17 @@
+public class SubArray {
+	static void PrintSubarray(int []a) {
+		for(int i=0;i<a.length;i++) {
+			for(int j = i; j<a.length;j++) {
+				for(int k=i;k<=j;k++) {
+					System.out.print(a[k]+" ");
+				}
+				System.out.println();
+			}
+		}
+	}
+	
+public static void main (String[]args) {
+	int [] a = {1,2,3,4,5,6,7};
+	PrintSubarray(a);
+}
+}
